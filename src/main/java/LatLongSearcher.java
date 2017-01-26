79d1f2f9f29508e17e4b2d031712507bf40c7aeb
Corin Mulliss <Corin.Mulliss-CIC-UK@uk.ibm.com>
2017-01-26 22:48:57 +0000

    createTree() now works

--- src/main/java/LatLongSearcher.java
+++ src/main/java/LatLongSearcher.java
@@ -1,53 +1,101 @@
 import net.sf.javaml.core.kdtree.KDTree;
 import org.json.simple.JSONArray;
-import org.json.simple.JSONAware;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.util.LinkedList;
-import java.util.ListIterator;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class LatLongSearcher {
 
     private JSONParser parser;
     private KDTree tree;
+    private String metApiKey;
 
     LatLongSearcher() {
         this.parser = new JSONParser();
+        this.tree = new KDTree(2);
+        this.metApiKey = System.getenv().get("MET_KEY");
     }
 
-    public String createTree(String json) {
-
-        String toPrint = "Nothing here yet";
+    public void createTree() {
 
         try {
-            // Get array of locations from JSON
-            Object object = parser.parse(json);
-            JSONArray jsonArray = (JSONArray) object;
-            // JSONObject jsonObject = (JSONObject) object;
-            toPrint = jsonArray.toString();
+            JSONArray rawMetLocations = this.retrieveMetOfficeLocations();
 
+            // Turn rawMetLocations into JSON needed by original createTree function
+            for (Object rawMetLocation : rawMetLocations) {
+                this.insertIntoTree((JSONObject) rawMetLocation);
+            }
+
+        } catch (UnirestException e) {
+            e.printStackTrace();
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        // Create tree, store in private property
-            // tree.insert([lat, long], object);
-        return toPrint;
 
     }
 
-    // public int searchMetLocations(Location currentLocation) {
+    public int searchMetLocations(String postcode) {
 
-    //     int result = 0;
+        // Look up lat and long for postcode
+        // Search in tree
 
-    //     double[] search = new double[2];
-    //     search[0] = currentLocation.getLatitudeActual();
-    //     search[1] = currentLocation.getLongitudeActual();
+        // int result = 0;
 
-    //     result = (int) tree.nearest(search);
+        // double[] search = new double[2];
+        // search[0] = currentLocation.getLatitudeActual();
+        // search[1] = currentLocation.getLongitudeActual();
 
-    //     return result;
-    // }
+        // result = (int) tree.nearest(search);
+
+        // return result;
+        return 0;
+    }
+
+
+    /*
+     * Requests Met Office locations from Met Office API, returns JSONArray
+     */
+    private JSONArray retrieveMetOfficeLocations() throws UnirestException, ParseException {
+        HttpResponse<JsonNode> jsonResponse = Unirest.get("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist")
+            .queryString("key", this.metApiKey)
+            .asJson();
+            
+        String rawBody = jsonResponse.getBody().toString();
+        JSONObject body = (JSONObject) this.parser.parse(rawBody);
+        JSONObject bodyLocations = (JSONObject) body.get("Locations");
+        JSONArray rawMetLocations = (JSONArray) bodyLocations.get("Location");
+        return rawMetLocations;
+    }
+
+
+    /*
+     * Given a raw Met Office location in JSONObject form, inserts it into KDTree
+     */
+    private void insertIntoTree(JSONObject rawMetLocation) {
+        double latitude = Double.parseDouble(rawMetLocation.get("latitude").toString());
+        double longitude = Double.parseDouble(rawMetLocation.get("longitude").toString());
+        String name = rawMetLocation.get("name").toString();
+        String id = rawMetLocation.get("id").toString();
+        String area = rawMetLocation.get("unitaryAuthArea").toString();
+
+        // Build new JSON object
+        JSONObject location = new JSONObject();
+        location.put("id", id);
+        location.put("name", name);
+        location.put("area", area);
+
+        double[] key = new double[2];
+        key[0] = latitude;
+        key[1] = longitude;
+
+        this.tree.insert(key, location);
+    }
+
+    
 
 }
