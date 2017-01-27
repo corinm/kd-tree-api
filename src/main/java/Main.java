e01191df4b83c8c0d09deb6ee046fd21d21be7ba
Corin Mulliss <Corin.Mulliss-CIC-UK@uk.ibm.com>
2017-01-27 13:01:44 +0000

    Added /db-test route

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,8 @@
+import java.sql.*;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 import com.heroku.sdk.jdbc.DatabaseUrl;
@@ -17,6 +22,33 @@ public class Main {
     /*
      * API Routes
      */
+
+    get("/db-test", (req, res) -> {
+      Connection connection = null;
+      Map<String, Object> attributes = new HashMap<>();
+      try {
+        connection = DatabaseUrl.extract().getConnection();
+
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+        ArrayList<String> output = new ArrayList<String>();
+        while (rs.next()) {
+          output.add( "Read from DB: " + rs.getTimestamp("tick"));
+        }
+
+        attributes.put("results", output);
+        // return new ModelAndView(attributes, "db.ftl");
+      } catch (Exception e) {
+        attributes.put("message", "There was an error: " + e);
+        // return new ModelAndView(attributes, "error.ftl");
+      } finally {
+        if (connection != null) try{connection.close();} catch(SQLException e){}
+        return "";
+      }
+    });
 
     /*
      * POST: /tree/create - Creates a new tree, returns id of tree for later access
