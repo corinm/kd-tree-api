26a765bda380fc4ab179fb2e1f37b9d33de3dea2
Corin Mulliss <Corin.Mulliss-CIC-UK@uk.ibm.com>
2017-01-28 14:41:25 +0000

    Minor change

--- src/main/java/Serialiser.java
+++ src/main/java/Serialiser.java
@@ -9,38 +9,19 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
- 
-/**
- * This class is a utility class for performing the serialization and
- * deserialization operations provided the required information.
- *
- * @author hiteshgarg
- */
-public class Serialiser {
- 
-    /**
-     * deserialize to Object from given file. We use the general Object so as
-     * that it can work for any Java Class.
-     */
-    public static Object deserialise(String fileName) throws IOException,
-            ClassNotFoundException {
-        FileInputStream fis = new FileInputStream(fileName);
-        BufferedInputStream bis = new BufferedInputStream(fis);
-        ObjectInputStream ois = new ObjectInputStream(bis);
-        Object obj = ois.readObject();
-        ois.close();
-        return obj;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang.SerializationUtils;
+
+class Serialiser {
+
+    static byte[] serialise(Serializable obj) {
+        return SerializationUtils.serialize(obj);
     }
- 
-    /**
-     * serialize the given object and save it to given file
-     */
-    public static void serialise(Object obj, String fileName)
-            throws IOException {
-        FileOutputStream fos = new FileOutputStream(fileName);
-        BufferedOutputStream bos = new BufferedOutputStream(fos);
-        ObjectOutputStream oos = new ObjectOutputStream(bos);
-        oos.writeObject(obj);
-        oos.close();
+
+    static Object deserialise(byte[] bytes) {
+        return SerializationUtils.deserialize(bytes);
     }
+
 }
