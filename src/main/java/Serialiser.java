612bb5afc886a074a9ea8b6d8de41a02d00612ec
Corin Mulliss <Corin.Mulliss-CIC-UK@uk.ibm.com>
2017-01-28 14:05:56 +0000

    Changed serialise methods

--- src/main/java/Serialiser.java
+++ src/main/java/Serialiser.java
@@ -10,14 +10,14 @@ import java.io.ObjectOutputStream;
 
 public class Serialiser {
 
-    public static byte[] serialize(Object obj) throws IOException {
+    public static byte[] serialise(Object obj) throws IOException {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         ObjectOutputStream os = new ObjectOutputStream(out);
         os.writeObject(obj);
         return out.toByteArray();
     }
-    
-    public static Object deserialize(byte[] data) throws IOException, ClassNotFoundException {
+
+    public static Object deserialise(byte[] data) throws IOException, ClassNotFoundException {
         ByteArrayInputStream in = new ByteArrayInputStream(data);
         ObjectInputStream is = new ObjectInputStream(in);
         return is.readObject();
